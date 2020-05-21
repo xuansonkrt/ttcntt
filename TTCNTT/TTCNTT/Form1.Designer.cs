d9ac3beb3f5e06158c31e8b53326354d2b7fa8a1
xuansonkrt <xuansonkrt@gmail.com>
2020-05-21 21:00:30 +0700

    init

---
+++ TTCNTT/TTCNTT/Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace TTCNTT
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBoxInfo = new System.Windows.Forms.GroupBox();
+            this.groupBoxResult = new System.Windows.Forms.GroupBox();
+            this.SuspendLayout();
+            // 
+            // groupBoxInfo
+            // 
+            this.groupBoxInfo.Location = new System.Drawing.Point(13, 13);
+            this.groupBoxInfo.Name = "groupBoxInfo";
+            this.groupBoxInfo.Size = new System.Drawing.Size(1578, 155);
+            this.groupBoxInfo.TabIndex = 0;
+            this.groupBoxInfo.TabStop = false;
+            this.groupBoxInfo.Text = "Nhập thông tin";
+            // 
+            // groupBoxResult
+            // 
+            this.groupBoxResult.Location = new System.Drawing.Point(12, 174);
+            this.groupBoxResult.Name = "groupBoxResult";
+            this.groupBoxResult.Size = new System.Drawing.Size(1579, 588);
+            this.groupBoxResult.TabIndex = 1;
+            this.groupBoxResult.TabStop = false;
+            this.groupBoxResult.Text = "Kết quả";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1603, 774);
+            this.Controls.Add(this.groupBoxResult);
+            this.Controls.Add(this.groupBoxInfo);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        public System.Windows.Forms.GroupBox groupBoxInfo;
+        public System.Windows.Forms.GroupBox groupBoxResult;
+    }
+}
+
---
+++ TTCNTT/TTCNTT/Form1.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.Msagl.Drawing;
+
+namespace TTCNTT
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            //create a form 
+           // System.Windows.Forms.Form form = new System.Windows.Forms.Form();
+            //create a viewer object 
+            Microsoft.Msagl.GraphViewerGdi.GViewer viewer = new Microsoft.Msagl.GraphViewerGdi.GViewer();
+            //create a graph object 
+            Microsoft.Msagl.Drawing.Graph graph = new Microsoft.Msagl.Drawing.Graph("graph");
+            //create the graph content 
+            graph.AddEdge("A", "B");
+            graph.AddEdge("B", "A");
+            graph.AddEdge("B", "C");
+
+            graph.AddEdge("1", "6");
+            graph.AddEdge("2", "5");
+            graph.AddEdge("3", "2");
+            graph.AddEdge("4", "1");
+            graph.AddEdge("5", "2");
+            graph.AddEdge("6", "2");
+            graph.AddEdge("7", "4");
+            graph.AddEdge("8", "10");
+            graph.AddEdge("9", "9");
+            graph.AddEdge("10", "6");
+            graph.AddEdge("1", "5");
+            graph.AddEdge("2", "3");
+            graph.AddEdge("3", "6");
+            graph.AddEdge("4", "7");
+            Node node = new Node("111");
+            node.Attr.Shape = Shape.Circle;
+            graph.AddNode(node);
+            graph.AddEdge("A", "C").Attr.Color = Microsoft.Msagl.Drawing.Color.Green;
+            Edge edge = new Edge("14","sonnx","15");
+            
+            graph.AddEdge("14", "sonnx", "15");
+            graph.FindNode("A").Attr.FillColor = Microsoft.Msagl.Drawing.Color.Magenta;
+            graph.FindNode("B").Attr.FillColor = Microsoft.Msagl.Drawing.Color.MistyRose;
+            Microsoft.Msagl.Drawing.Node c = graph.FindNode("C");
+            c.Attr.FillColor = Microsoft.Msagl.Drawing.Color.PaleGreen;
+            c.Attr.Shape = Microsoft.Msagl.Drawing.Shape.Circle;
+            //bind the graph to the viewer 
+            viewer.Graph = graph;
+            //associate the viewer with the form 
+            this.groupBoxResult.SuspendLayout();
+            viewer.Dock = System.Windows.Forms.DockStyle.Fill;
+            
+            // this.Controls.Add(viewer);
+            this.groupBoxResult.Controls.Add(viewer);
+            this.groupBoxResult.ResumeLayout();
+            //show the form 
+           // this.ShowDialog();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
